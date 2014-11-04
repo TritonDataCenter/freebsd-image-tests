e1c393a8f078e96b116991668350cca62d35bfa1
Christopher Horrell <christopher@joyent.com>
2014-11-04 19:19:16 +0000

    Fixed product file and MOTD check

--- spec/freebsd/product_and_branding_files_spec.rb
+++ spec/freebsd/product_and_branding_files_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 
-# Files that should be installed
-
 # product
 describe file('/etc/product') do
 	it { should be_file }
@@ -10,22 +8,10 @@ describe file('/etc/product') do
 	it { should contain "Documentation: #{property[:doc_url]}" }
 end
 
-## MOTD
-# Since 2.6.1. motd is just a static file
+# MOTD
 describe file('/etc/motd') do
   it { should be_file }
   it { should contain "Instance (#{property[:name]} #{property[:version]}" }
   it { should contain "#{property[:doc_url]}" }
 end
 
-# Since 2.6.1, link to the actual /etc/motd file
-describe file('/etc/issue') do
-  it { should be_file }
-  it { should be_linked_to '/etc/motd' }
-end
-
-# Since 2.6.1, link to the actual /etc/motd file
-describe file('/etc/issue.net') do
-    it { should be_file }
-    it { should be_linked_to '/etc/motd' }
-end
