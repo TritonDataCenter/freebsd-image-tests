c974674c703d01ebcc282f1d5b53c1c086ceee1c
Christopher Horrell <christopher@joyent.com>
2014-11-04 18:45:18 +0000

    Make sure root can log in

--- spec/freebsd/rootlogin_spec.rb
+++ spec/freebsd/rootlogin_spec.rb
@@ -11,3 +11,8 @@ describe file('/etc/ssh/sshd_config') do
   it { should be_file }
   it { should contain "PasswordAuthentication no" }
 end
+
+describe file('/etc/ssh/sshd_config') do
+  it { should be_file }
+  it { should contain "PermitRootLogin yes" }
+end
