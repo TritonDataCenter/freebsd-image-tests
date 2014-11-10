f85dfb80677a1c0920897164ebec988d24cd86b3
Christopher Horrell <christopher@joyent.com>
2014-11-10 19:19:42 +0000

    Interfaces set dyamically, ensure smartdc_enable is enabled and check for hostname

--- spec/freebsd/rc.conf_spec.rb
+++ spec/freebsd/rc.conf_spec.rb
@@ -6,10 +6,10 @@ describe file('/etc/rc.conf') do
   it { should be_file }
   it { should contain "fsck_y_enable=\"YES\"" }
 	it { should contain "dumpdev=\"AUTO\"" }
-	it { should contain "ifconfig_vtnet0=\"DHCP\"" }
-	it { should contain "ifconfig_vtnet1=\"DHCP\"" }
+  it { should contain "smartdc_enable=\"YES\"" }
 	it { should contain "sshd_enable=\"YES\"" }
 	it { should contain "ntpd_enable=\"YES\"" }
 	it { should contain "ntpd_sync_on_start=\"YES\"" }
+  it { should contain "hostname=" }
 end
 
