982b7085cb7bbf9da03c7add00c55a6d565fb70d
Christopher Horrell <christopher@joyent.com>
2015-08-14 11:01:51 -0400

    10.2 has better defaults for ntpd

--- spec/freebsd/ntp_spec.rb
+++ spec/freebsd/ntp_spec.rb
@@ -4,8 +4,8 @@ require 'spec_helper'
 
 describe file('/etc/ntp.conf') do
   it { should be_file }
-	it { should contain "restrict default kod nomodify notrap nopeer noquery" }
-	it { should contain "restrict -6 default kod nomodify notrap nopeer noquery" }
+	it { should contain "restrict default limited kod nomodify notrap nopeer noquery" }
+	it { should contain "restrict -6 default limited kod nomodify notrap nopeer noquery" }
   it { should contain "restrict 127.0.0.1" }
   it { should contain "restrict -6 ::1" }
 end
