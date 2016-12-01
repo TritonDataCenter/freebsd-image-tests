06abe94cc3b4919aa5e334429e79be7fd4a239ce
Christopher Horrell <christopher@joyent.com>
2016-12-01 14:02:06 -0500

    /lib/smartdc renamed to /lib/triton in FreeBSD 11.0 image

--- spec/freebsd/guest-tools_spec.rb
+++ spec/freebsd/guest-tools_spec.rb
@@ -1,49 +1,98 @@
 require 'spec_helper'
 
-# Ensure guest tools are installed in /lib/smartdc
+# Ensure guest tools are installed in
 
-describe file('/lib/smartdc/add-network-interface') do
-  it { should be_file }
-  it { should be_executable }
+# For FreeBSD 10, the guest tools were installed in /lib/smartdc
+# For FreeBSD 11, they are instaled in /lib/triton
+
+if property[:name].include? "FreeBSD 10.0"
+  describe file('/lib/smartdc/add-network-interface') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/smartdc/firstboot') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/smartdc/format-secondary-disk') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/smartdc/lib_smartdc_scripts.cfg') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/smartdc/run-operator-script') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/smartdc/run-user-script') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/smartdc/set-hostname') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/smartdc/set-root-authorized-keys') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/smartdc/set-rootpassword') do
+    it { should be_file }
+    it { should be_executable }
+  end
+else
+  describe file('/lib/triton/add-network-interface') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/triton/firstboot') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/triton/format-secondary-disk') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/triton/lib_triton_scripts.cfg') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/triton/run-operator-script') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/triton/run-user-script') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/triton/set-hostname') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/triton/set-root-authorized-keys') do
+    it { should be_file }
+    it { should be_executable }
+  end
+
+  describe file('/lib/triton/set-rootpassword') do
+    it { should be_file }
+    it { should be_executable }
+  end
 end
-
-describe file('/lib/smartdc/firstboot') do
-  it { should be_file }
-  it { should be_executable }
-end
-
-describe file('/lib/smartdc/format-secondary-disk') do
-  it { should be_file }
-  it { should be_executable }
-end
-
-describe file('/lib/smartdc/lib_smartdc_scripts.cfg') do
-  it { should be_file }
-  it { should be_executable }
-end
-
-describe file('/lib/smartdc/run-operator-script') do
-  it { should be_file }
-  it { should be_executable }
-end
-
-describe file('/lib/smartdc/run-user-script') do
-  it { should be_file }
-  it { should be_executable }
-end
-
-describe file('/lib/smartdc/set-hostname') do
-  it { should be_file }
-  it { should be_executable }
-end
-
-describe file('/lib/smartdc/set-root-authorized-keys') do
-  it { should be_file }
-  it { should be_executable }
-end
-
-describe file('/lib/smartdc/set-rootpassword') do
-  it { should be_file }
-  it { should be_executable }
-end
-
