51bcb9098cf5d88ad623eef2a5c3156a79393eea
Christopher Horrell <christopher@joyent.com>
2017-03-29 15:05:12 -0400

    Add new boot loader settings

--- spec/freebsd/loader.conf_spec.rb
+++ spec/freebsd/loader.conf_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
-# Tests to ensure ntp is locked down 
 
 describe file('/boot/loader.conf') do
   it { should be_file }
   it { should contain "autoboot_delay=\"5\"" }
+  it { should contain "verbose_loading=\"YES\"" }
+  it { should contain "hw.usb.disable_enumeration=1" }
+  it { should contain "hw.usb.no_boot_wait=1" }
+  it { should contain "hw.usb.no_shutdown_wait=1" }
 end
 
