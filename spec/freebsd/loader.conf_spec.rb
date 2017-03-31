28723a0d6e9268fd367c2b2df8622f89c8607071
Christopher Horrell <christopher@joyent.com>
2017-03-31 11:26:25 -0400

    Test for hw.snd.default_auto setting

--- spec/freebsd/loader.conf_spec.rb
+++ spec/freebsd/loader.conf_spec.rb
@@ -5,6 +5,7 @@ describe file('/boot/loader.conf') do
   it { should be_file }
   it { should contain "autoboot_delay=\"5\"" }
   it { should contain "verbose_loading=\"YES\"" }
+  it { should contain "hw.snd.default_auto=0" }
   it { should contain "hw.usb.disable_enumeration=1" }
   it { should contain "hw.usb.no_boot_wait=1" }
   it { should contain "hw.usb.no_shutdown_wait=1" }
